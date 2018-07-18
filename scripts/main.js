911d313d20bd015bb4a13603251ebe0f999303de
Vasily Romashin <teozad@gmail.com>
2018-07-18 15:29:09 +0300

    js udemy

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,56 @@
-console.log('Hello World!')
+//LECTURE 2-8
+//BASIC
+
+var firstName = "Jonh";
+console.log(firstName);
+
+var lastName = "Smith";
+var age = 26;
+
+var fullAge = true;
+
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = "Teacher";
+console.log(job);
+
+// LECTURE 2-9
+// Object mutations
+
+var firstName = "Jonh";
+var age = 28;
+
+console.log(firstName + " " + age);
+
+var job, isMarried;
+job = "Teacher";
+isMarried = false;
+
+console.log(
+	firstName +
+		" is a " +
+		age +
+		" year old" +
+		job +
+		". Is he married? " +
+		isMarried
+);
+
+// var mutation
+age = "twenty eight";
+
+console.log(
+	firstName +
+		" is a " +
+		age +
+		" year old" +
+		job +
+		". Is he married? " +
+		isMarried
+);
+
+// LECTURE 2-10
+// Basic operators

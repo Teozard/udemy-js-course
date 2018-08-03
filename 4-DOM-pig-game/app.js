ee2fbd41f813b269fd7429983c71ca029c40540b
Teozard <teozard@gmail.com>
2018-08-03 18:51:01 +0300

    add input field to set winngin score, however still need some fixes

--- 4-DOM-pig-game/app.js
+++ 4-DOM-pig-game/app.js
@@ -9,7 +9,7 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice, gamePlaying;
+var scores, roundScore, activePlayer, dice, gamePlaying, previusRoll, winScore;
 
 init();
 
@@ -18,6 +18,8 @@ document.querySelector(".btn-roll").addEventListener("click", function() {
 	if (gamePlaying) {
 		// 1. Rand number
 		dice = Math.floor(Math.random() * 6) + 1;
+
+
 
 		// 2. Display the result
 		var diceDOM = document.querySelector(".dice");
@@ -39,6 +41,9 @@ document.querySelector(".btn-roll").addEventListener("click", function() {
 
 document.querySelector(".btn-hold").addEventListener("click", function() {
 	if (gamePlaying) {
+		// get win score value
+		winScore = document.querySelector('.input-field').value;
+
 		// add current score to global score
 		scores[activePlayer] += roundScore;
 
@@ -47,7 +52,7 @@ document.querySelector(".btn-hold").addEventListener("click", function() {
 			scores[activePlayer];
 
 		// check if player won the game
-		if (scores[activePlayer] >= 100) {
+		if (scores[activePlayer] >= winScore) {
 			document.querySelector("#name-" + activePlayer).textContent =
 				"Winner!";
 			document.querySelector(".dice").style.display = "none";
@@ -87,6 +92,7 @@ function init() {
 	roundScore = 0;
 	activePlayer = 0;
 	gamePlaying = true;
+	previusRoll = 0;
 
 	document.querySelector(".dice").style.display = "none";
 
@@ -110,13 +116,15 @@ function init() {
 /*
 YOUR 3 CHALLENGES
 
-1. A player looses his ENTIREscore when he rolls two 6 in a row. 
+1. A player looses his ENTIRE score when he rolls two 6 in a row. 
 After that, it's the next player's turn. (HINT: Always save the 
 previous dice rolls in a separate variable)
+
 
 2. Add an input field to the HTML where players can set the winning 
 score, so that they can change the predefined score of 100. (HINT: 
 you can read that value with the .value property in javascipt.)
+--DONE--
 
 3. Add another dice to the game, so that there are two dices now.
 The player looses his current score when one of them is a 1. 
---
+++ test.js
@@ -0,0 +1,5 @@
+var dice = Math.floor(Math.random() * 6) + 1;
+var secondDice = dice;
+
+console.log(dice);
+console.log(secondDice);

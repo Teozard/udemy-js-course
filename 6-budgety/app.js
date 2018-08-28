2a091f869d08ea8fc501618ca01135f36c92224f
Teozard <teozard@gmail.com>
2018-08-28 13:25:18 +0300

    delete an item from the UI

--- 6-budgety/app.js
+++ 6-budgety/app.js
@@ -158,6 +158,11 @@ var UIController = (function () {
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
 
+        deleteListItem: function(selectorID) {
+            var el = document.getElementById(selectorID);
+            el.parentNode.removeChild(el);
+        },
+
         clearFields: function () {
             var fields, fieldsArr;
             fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
@@ -263,9 +268,10 @@ var controller = (function (budgetCtrl, UICtrl) {
             budgetCtrl.deleteItem(type, ID);
 
             // 2. delete item from the UI
+            UICtrl.deleteListItem(itemID);
 
             // 3. update and show the new budget
-
+            updateBudget();
         }
 
     };
